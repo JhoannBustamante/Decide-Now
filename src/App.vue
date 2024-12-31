acf9973eaa8020f304823837258ba7808614d6c5
JhoannBustamante <JhoannBustamanteFP@gmail.com>
2024-12-30 23:30:35 -0500

    delete compo inutiles y create listadetarea

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
 <contador></contador>
+<ListaDeTareas></ListaDeTareas>
 </template>
 
 <script setup>
-import Contador from './components/contador.vue'
+import Contador from './components/Contador.vue'
+import ListaDeTareas from './components/ListaDeTareas.vue'
 </script>
 
 <style scoped>
